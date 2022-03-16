d500aa2aebcd49b7b8eccb5dc86c8c5c95562098
William Phelps <wphelps@quarteroniongames.com>
2022-03-16 16:47:26 -0400

    Updated settings to contain only what is required for the bot to function

--- XRPLMarketMaker/Settings.cs
+++ XRPLMarketMaker/Settings.cs
@@ -11,11 +11,7 @@ namespace EMBRS
         public static string CurrencyCode { get; set; }
         public static string IssuerAddress { get; set; }
         public static int BotTokenAmt { get; set; }
-        public static int AccountLinesThrottle { get; set; }
         public static int TxnThrottle { get; set; }
-        public static decimal FeeMultiplier { get; set; }
-        public static int MaximumFee { get; set; }
-        public static decimal TransferFee { get; set; }
         public static int Steps { get; set; }
         public static decimal Interval { get; set; }
 
@@ -30,12 +26,8 @@ namespace EMBRS
             if(currencyCodeVal.Length == 3) CurrencyCode = d.CurrencyCode.Value;
             else CurrencyCode = Utils.AddZeros(Utils.ConvertHex(d.CurrencyCode.Value));
             IssuerAddress = d.IssuerAddress;
-            TransferFee = d.TransferFee;
             BotTokenAmt = d.BotTokenAmt;
-            AccountLinesThrottle = d.AccountLinesThrottle;
             TxnThrottle = d.TxnThrottle;
-            FeeMultiplier = d.FeeMultiplier;
-            MaximumFee = d.MaximumFee;
             Steps = d.Steps;
             Interval = d.Interval;
         }
